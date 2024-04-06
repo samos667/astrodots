c6d055e8a548ea9554fd180f5427d306f5f0fe69
samos667 <samos667@setur.cc>
2024-04-07 00:55:49 +0200

    fix: fix catpuccin

--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -1,4 +1,12 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      opts = function(_, opts)
+        opts.flavour = "mocha"             -- latte, frappe, macchiato, mocha
+        opts.transparent_background = true -- setting the background color.
+      end,
+    },
 }
