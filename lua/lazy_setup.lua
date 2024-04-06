a852bbf702b27f6c2a002dc71e4a4c316f7353bc
samos667 <samos667@setur.cc>
2024-04-07 00:47:52 +0200

    feat: fix lazy-lock on nixos

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
   { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
+  lockfile = vim.fn.stdpath("config") .. "../lazy-lock.json", -- lockfile generated after running update.
   install = { colorscheme = { "astrodark", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
