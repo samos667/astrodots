800ebc0a79dd7e8829fcce0eb10770f3768f0620
Sammy ETUR <sammy.etur-ext@beebryte.com>
2024-04-08 16:35:30 +0200

    feat: setup nu linter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,10 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "nushell/tree-sitter-nu" },
+  },
+  build = ":TSUpdate",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
